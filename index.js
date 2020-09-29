e3d86be5b50fc05bb596c395c2a763dc26224df2
HugoLattard <hugolattard@gmail.com>
2020-09-29 17:03:38 +0200

    new config vars

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ app.use(bodyParser.json());
 
 // DataBase config
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+// mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error data base connection:"));
 db.once("open", function () {
-      console.log("Connected to data base");
+  console.log("Connected to data base");
 });
 
 // Loading models
@@ -32,52 +33,52 @@ const uploadPicture = require("./UploadPicture");
 // Routes =======================================
 
 app.get("/", (req, res) => {
-      res.status(200).json("Bienvenue sur mon site infos");
+  res.status(200).json("Bienvenue sur mon site infos");
 });
 
 // Route create picture =========================
 
 app.post(
-      "/picture/publish",
-      formidableMiddleware(),
-      uploadPicture,
-      async (req, res) => {
-            // res.status(200).json({ message: "route create picture OK" });
-            console.log("route create picture OK");
+  "/picture/publish",
+  formidableMiddleware(),
+  uploadPicture,
+  async (req, res) => {
+    // res.status(200).json({ message: "route create picture OK" });
+    console.log("route create picture OK");
 
-            try {
-                  const name = req.fields.name;
-                  const picture = new Picture({ name });
-                  picture.url = req.pictures;
-                  await picture.save();
-                  res.status(200).json(picture);
-                  console.log("Upload picture OK");
-            } catch (error) {
-                  console.error(error);
-                  res.status(400).json({ message: error.message });
-            }
-      }
+    try {
+      const name = req.fields.name;
+      const picture = new Picture({ name });
+      picture.url = req.pictures;
+      await picture.save();
+      res.status(200).json(picture);
+      console.log("Upload picture OK");
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: error.message });
+    }
+  }
 );
 
 // Route create picture =========================
 
 app.get("/picture", async (req, res) => {
-      // res.status(200).json({ message: "route read picture OK" });
-      console.log("route read picture OK");
-      try {
-            const picture = await Picture.find();
-            if (picture) {
-                  res.status(200).json(picture);
-                  console.log("read picture OK");
-            } else {
-                  res.status(404).json({ message: "Image not found" });
-            }
-      } catch (error) {
-            console.error(error);
-            res.status(400).json({ message: error.message });
-      }
+  // res.status(200).json({ message: "route read picture OK" });
+  console.log("route read picture OK");
+  try {
+    const picture = await Picture.find();
+    if (picture) {
+      res.status(200).json(picture);
+      console.log("read picture OK");
+    } else {
+      res.status(404).json({ message: "Image not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(process.env.PORT, () => {
-      console.log("Server has started on port " + process.env.PORT);
+  console.log("Server has started on port " + process.env.PORT);
 });
